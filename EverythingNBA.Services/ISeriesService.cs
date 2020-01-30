26bbae2930960c3d3713e430d51a7c1c5098164a
Antonio <antniopatsov477@gmail.com>
2020-01-30 16:32:38 +0200

    Added initial Series services

---
+++ EverythingNBA.Services/ISeriesService.cs
@@ -0,0 +1,13 @@
+﻿namespace EverythingNBA.Services
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface ISeriesService
+    {
+        Task<int> AddSeriesAsync(int team1Id, int team2Id, int winnerGamesWon, int loserGamesWon, int game1Id, int game2Id, int game3Id, 
+            int game4Id, int? game5Id, int? game6Id, int? game7Id);
+
+        Task<bool> DeleteSerieAsync (int seriesId);
+    }
+}
---
+++ EverythingNBA.Services/Implementations/SeriesService.cs
@@ -0,0 +1,56 @@
+﻿namespace EverythingNBA.Services.Implementations
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using EverythingNBA.Data;
+    using EverythingNBA.Models;
+
+    public class SeriesService : ISeriesService
+    {
+        private readonly EverythingNBADbContext db;
+
+        public SeriesService(EverythingNBADbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<int> AddSeriesAsync(int team1Id, int team2Id, int winnerGamesWon, int loserGamesWon, int game1Id, int game2Id, int game3Id, int game4Id, int game5Id, int game6Id, int game7Id)
+        {
+            var seriesObj = new Series
+            {
+                Team1Id = team1Id,
+                Team2Id =team2Id,
+                WinnerGamesWon = winnerGamesWon,
+                LoserGamesWon = loserGamesWon,
+                Game1Id = game1Id,
+                Game2Id = game2Id,
+                Game3Id = game3Id,
+                Game4Id = game4Id,
+                Game5Id = game5Id,
+                Game6Id = game6Id,
+                Game7Id = game7Id
+            };
+
+            this.db.Series.Add(seriesObj);
+            await this.db.SaveChangesAsync();
+
+            return seriesObj.Id;
+        }
+
+        public async Task<bool> DeleteSerieAsync(int seriesId)
+        {
+            var seriesToDelete = await this.db.Series.FindAsync(seriesId);
+
+            if (seriesToDelete == null)
+            {
+                return false;
+            }
+
+            this.db.Series.Remove(seriesToDelete);
+            await this.db.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
