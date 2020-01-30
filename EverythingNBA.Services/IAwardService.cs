4b17b3631741a525cddec70a2fd9224ffabd29e6
Antonio <antniopatsov477@gmail.com>
2020-01-30 15:37:41 +0200

    Added initial award services

---
+++ EverythingNBA.Services/IAwardService.cs
@@ -0,0 +1,12 @@
+﻿namespace EverythingNBA.Services
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IAwardService
+    {
+        Task<int> AddAwardAsync(string name, int year, int winnerId);
+
+        Task<bool> DeleteAwardAsync(int awardId);
+    }
+}
