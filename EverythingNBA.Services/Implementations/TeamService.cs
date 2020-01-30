7f4e4fe366eb6b4e343d3a42018dfc866ec9f87d
Antonio <antniopatsov477@gmail.com>
2020-01-30 12:21:37 +0200

    Added Add and Delete Services

--- EverythingNBA.Services/Implementations/TeamService.cs
+++ EverythingNBA.Services/Implementations/TeamService.cs
@@ -1,10 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EverythingNBA.Services.Implementations
+﻿namespace EverythingNBA.Services.Implementations
 {
-    class TeamService
+    using EverythingNBA.Data;
+    using EverythingNBA.Models;
+    using EverythingNBA.Models.Enums;
+
+    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.Threading.Tasks;
+
+    public class TeamService : ITeamService
     {
+        private readonly EverythingNBADbContext db;
+        private readonly IImageService imageService;
+
+        public TeamService(EverythingNBADbContext db, IImageService imageService)
+        {
+            this.db = db;
+            this.imageService = imageService;
+        }
+
+        public async Task<int> AddTeamAsync(string name, IFormFile imageFile, string conference, string venue, string instagram, string twitter)
+        {
+            var imageId = await this.imageService.UploadImageAsync(imageFile);
+
+            var teamObj = new Team
+            {
+                Name = name,
+                CloudinaryImageId = imageId,
+                Conference = (ConferenceType)Enum.Parse(typeof(ConferenceType), conference),
+                Venue = venue,
+                Twitter = twitter,
+                Instagram = instagram
+            };
+
+            this.db.Teams.Add(teamObj);
+            await this.db.SaveChangesAsync();
+
+            return teamObj.Id;
+        }
+
+        public async Task<bool> DeleteTeamAsync(int teamId)
+        {
+            var teamToDelete = await this.db.Teams.FindAsync(teamId);
+
+            if (teamToDelete == null)
+            {
+                return false;
+            }
+
+            this.db.Teams.Remove(teamToDelete);
+
+            await this.db.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
