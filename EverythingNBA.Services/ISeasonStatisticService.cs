5f19d20e2263c8c3c5810a3a5cd0044cf251bbd9
Antonio <antniopatsov477@gmail.com>
2020-01-30 17:07:37 +0200

    Added SeasonStatistic initial services

---
+++ EverythingNBA.Services/ISeasonStatisticService.cs
@@ -0,0 +1,12 @@
+﻿namespace EverythingNBA.Services
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface ISeasonStatisticService
+    {
+        Task<int> AddAsync(int seasonId, int wins, int losses);
+
+        Task<bool> DeleteAsync(int seasonStatisticId);
+    }
+}
---
+++ EverythingNBA.Services/Implementations/SeasonStatisticService.cs
@@ -0,0 +1,48 @@
+﻿namespace EverythingNBA.Services.Implementations
+{
+    using System.Threading.Tasks;
+    using System;
+
+    using EverythingNBA.Data;
+    using EverythingNBA.Models;
+
+    public class SeasonStatisticService : ISeasonStatisticService;
+    {
+        private readonly EverythingNBADbContext db;
+
+        public SeasonStatisticService(EverythingNBADbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<int> AddAsync(int seasonId, int wins, int losses)
+        {
+            var seasonStatisticObj = new SeasonStatistic
+            {
+                SeasonId = seasonId,
+                Wins = wins,
+                Losses = losses
+            };
+
+            this.db.SingleSeasonStatistics.Add(seasonStatisticObj);
+            await this.db.SaveChangesAsync();
+
+            return seasonStatisticObj.Id;
+        }
+
+        public async Task<bool> DeleteAsync(int seasonStatisticId)
+        {
+            var statisticToDelete = await this.db.SingleSeasonStatistics.FindAsync(seasonStatisticId);
+
+            if (statisticToDelete == null)
+            {
+                return false;
+            }
+
+            this.db.SingleSeasonStatistics.Remove(statisticToDelete);
+            await this.db.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
