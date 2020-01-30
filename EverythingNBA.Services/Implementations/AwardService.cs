3f6d2a8c94004e8a0809274c9578f5be51aeb616
Antonio <antniopatsov477@gmail.com>
2020-01-30 15:40:59 +0200

    Added initial AllStarTeam services

--- EverythingNBA.Services/Implementations/AwardService.cs
+++ EverythingNBA.Services/Implementations/AwardService.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace EverythingNBA.Services.Implementations
 {
-    interface AwardService
+    using EverythingNBA.Data;
+    using EverythingNBA.Models;
+    using EverythingNBA.Models.Enums;
+
+    using System;
+    using System.Threading.Tasks;
+
+    public class AwardService : IAwardService
     {
+        private readonly EverythingNBADbContext db;
+
+        public AwardService(EverythingNBADbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<int> AddAwardAsync(string name, int year, int winnerId)
+        {
+            var awardObj = new Award
+            {
+                Name = (AwardType)Enum.Parse(typeof(AwardType), name),
+                Year = year,
+                WinnerId = winnerId
+            };
+
+            this.db.Awards.Add(awardObj);
+            await this.db.SaveChangesAsync();
+
+            return awardObj.Id;
+        }
+
+        public async Task<bool> DeleteAwardAsync(int awardId)
+        {
+            var awardtoDelete = await this.db.Awards.FindAsync(awardId);
+
+            if (awardtoDelete == null)
+            {
+                return false;
+            }
+
+            this.db.Remove(awardtoDelete);
+            await this.db.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
