f98c7351da592e646c4303995849ed026a28d6af
Antonio <antniopatsov477@gmail.com>
2020-01-30 12:12:01 +0200

    Added Initial Team Services.

---
+++ EverythingNBA.Services/ITeamService.cs
@@ -0,0 +1,12 @@
+﻿namespace EverythingNBA.Services
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Threading.Tasks;
+
+    public interface ITeamService
+    {
+        Task<int> AddTeamAsync(string name, IFormFile imageFile, string conference, string venue, string instagram, string twitter);
+
+        Task<bool> DeleteTeamAsync(int teamId);
+    }
+}
