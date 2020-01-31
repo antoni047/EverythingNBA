f1e077d9b1eb9b3fe4fd18c5ec463f414d74e5e9
Antonio <antniopatsov477@gmail.com>
2020-01-31 12:18:28 +0200

    Added automapper configuration class library.

---
+++ EverythingNBA.Services.Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace EverythingNBA.Services.Mapping
+{
+    using AutoMapper;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+            // Additional mappings here...
+        }
+    }
+}
