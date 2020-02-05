849db0d294a9831329921a588768a91131f0095a
Antonio <antoniopatsov477@gmail.com>
2020-02-05 12:13:08 +0200

    Added player overview and team details service model

---
+++ EverythingNBA.Services/Models/Player/PlayerOverviewServiceModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EverythingNBA.Services.Models.Player
+{
+    public class PlayerOverviewServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double PointsPerGame { get; set; }
+
+        public double AssistsPerGame { get; set; }
+
+        public double ReboundsPerGame { get; set; }
+
+        public string Position { get; set; }
+    }
+}
---
+++ EverythingNBA.Services/Models/Team/GetTeamDetailsServiceModel.cs
@@ -0,0 +1,32 @@
+﻿namespace EverythingNBA.Services.Models.Team
+{
+    using EverythingNBA.Models;
+    using EverythingNBA.Services.Models.Player;
+
+    using System.Collections.Generic;
+
+    public class GetTeamDetailsServiceModel
+    {
+        public string Name { get; set; }
+
+        public string AbbreviatedName { get; set; }
+
+        public int? CloudinaryImageId { get; set; }
+
+        public string Conference { get; set; }
+
+        public string Venue { get; set; }
+
+        public string Instagram { get; set; }
+
+        public string Twitter { get; set; }
+
+        public SeasonStatistic CurrentSeasonStatistic { get; set; }
+
+        public ICollection<Game> CurrentSeasonGames { get; set; }
+
+        public ICollection<PlayerOverviewServiceModel> CurrentPlayers { get; set; }
+
+        public ICollection<int> TitlesWon { get; set; }
+    }
+}
