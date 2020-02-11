79210310248e97d8bfc349c3f859b806c026aa4b
Antonio <antniopatsov477@gmail.com>
2020-02-11 11:27:07 +0200

    Renamed SeasonStatistic and GameStatistic.

---
+++ EverythingNBA.Data/Migrations/20200210092932_RenamedSeasonStatisticsAndGameStatistics.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EverythingNBA.Data.Migrations
+{
+    public partial class RenamedSeasonStatisticsAndGameStatistics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
