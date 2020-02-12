9950c52f31b11ae30ba7dde56bdc2792b890c295
Antonio <antniopatsov477@gmail.com>
2020-02-12 16:57:47 +0200

    Added a PlayerGameStatistic ServiceModel.

---
+++ EverythingNBA.Services/Models/GameStatisticModels/PlayerGameStatisticServiceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EverythingNBA.Services.Models.GameStatistic
+{
+    public class PlayerGameStatisticServiceModel
+    {
+        public int PlayerId { get; set; }
+
+        public string StatisticType { get; set; }
+
+        public int Value { get; set; }
+    }
+}
