4b91e7b0669ef49e40bb2261f5cd23acd6257378
Antonio <antoniopatsov477@gmail.com>
2020-03-03 19:09:10 +0200

    Created mapping profile for web project

---
+++ EverythingNBA.Web/Mapping/Mapping.cs
@@ -0,0 +1,19 @@
+﻿namespace EverythingNBA.Web.Mapping
+{
+    using AutoMapper;
+    using System;
+
+    public class Mapping
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<MappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
---
+++ EverythingNBA.Web/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace EverythingNBA.Web.Mapping
+{
+    using AutoMapper;
+    using Models.Seasons;
+    using Services.Models.Season;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            this.CreateMap<SeasonDetailsInputModel, GetSeasonListingServiceModel>();
+        }
+    }
+}
