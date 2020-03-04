e2daa9bf78fd486910f7af2b8d98018be00fb5db
Antonio <antoniopatsov477@gmail.com>
2020-03-04 13:37:09 +0200

    Added initial AllStarTeam actions

--- EverythingNBA.Web/Controllers/AllStarTeamsController.cs
+++ EverythingNBA.Web/Controllers/AllStarTeamsController.cs
@@ -16,17 +16,30 @@
         private readonly IMapper mapper;
         private readonly EverythingNBADbContext db;
         private readonly IAllStarTeamService astService;
+        private readonly ISeasonService seasonService;
 
-        public AllStarTeamsController(IMapper mapper, EverythingNBADbContext db, IAllStarTeamService allStarTeamService)
+        public AllStarTeamsController(IMapper mapper, EverythingNBADbContext db, IAllStarTeamService allStarTeamService, ISeasonService seasonService)
         {
             this.mapper = mapper;
             this.db = db;
             this.astService = allStarTeamService;
+            this.seasonService = seasonService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> SeasonAllStarTeams(int seasonId)
         {
-            return View();
+            var year = await this.seasonService.GetYearAsync(seasonId);
+
+            var astTeams = await this.astService.GetAllASTeamsAsync(year);
+
+            return this.View(astTeams);
+        }
+
+        public async Task<IActionResult> AllStarTeamType(string type)
+        {
+            var astTeams = await this.astService.GetAllASTeamsAsync(type);
+
+            return this.View(astTeams);
         }
     }
 }
