e80093535337c6d14f3a0964a2a1c277030228c8
Antonio <antniopatsov477@gmail.com>
2020-03-06 18:34:50 +0200

    Added crud operations for teams

--- EverythingNBA.Web/Controllers/TeamsController.cs
+++ EverythingNBA.Web/Controllers/TeamsController.cs
@@ -17,12 +17,17 @@
         private readonly IMapper mapper;
         private readonly EverythingNBADbContext db;
         private readonly ITeamService teamService;
+        private readonly ISeasonService seasonService;
+        private readonly IPlayerService playerService;
 
-        public TeamsController(IMapper mapper, EverythingNBADbContext db, ITeamService teamService)
+        public TeamsController(IMapper mapper, EverythingNBADbContext db, ITeamService teamService, ISeasonService seasonService,
+            IPlayerService playerService)
         {
             this.db = db;
             this.mapper = mapper;
             this.teamService = teamService;
+            this.seasonService = seasonService;
+            this.playerService = playerService;
         }
         public async Task<IActionResult> All()
         {
@@ -63,20 +68,71 @@
             return RedirectToAction("All");
         }
 
-        private int GetCurrentSeasonYear()
+        [HttpGet]
+        public async Task<IActionResult> Edit(int teamId)
         {
-            var currentYear = 0;
+            var year = this.GetCurrentSeasonYear();
+            var teamModel = await this.teamService.GetTeamDetailsAsync(teamId, year);
 
-            if (DateTime.Now.Month >= 9)
+            if (teamModel == null)
             {
-                currentYear = DateTime.Now.Year + 1;
-            }
-            else if (DateTime.Now.Month < 9)
-            {
-                currentYear = DateTime.Now.Year;
+                return RedirectToAction("All");
             }
 
-            return currentYear;
+            return this.View(teamModel);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit (TeamInputModel inputModel, int teamId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View(inputModel);
+            }
+
+            var model = mapper.Map<GetTeamDetailsServiceModel>(inputModel);
+
+            await this.teamService.EditTeamAsync(model, teamId);
+
+            return RedirectToAction("TeamDetails");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete (int teamId)
+        {
+            var year = this.GetCurrentSeasonYear();
+            var team = await this.teamService.GetTeamDetailsAsync(teamId, year);
+
+            if (team == null)
+            {
+                return RedirectToAction("All");
+            }
+
+            return this.View(team);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int teamId, TeamInputModel model)
+        {
+            await this.teamService.DeleteTeamAsync(teamId);
+
+            return RedirectToAction("All");
+        }
+
+    private int GetCurrentSeasonYear()
+    {
+        var currentYear = 0;
+
+        if (DateTime.Now.Month >= 9)
+        {
+            currentYear = DateTime.Now.Year + 1;
+        }
+        else if (DateTime.Now.Month < 9)
+        {
+            currentYear = DateTime.Now.Year;
+        }
+
+        return currentYear;
     }
+}
 }
