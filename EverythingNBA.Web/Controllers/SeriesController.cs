33ec5b946a67f059f807fa094c932946cd990069
Antonio <antniopatsov477@gmail.com>
2020-03-06 13:27:35 +0200

    Added series intial action and crud operations

--- EverythingNBA.Web/Controllers/SeriesController.cs
+++ EverythingNBA.Web/Controllers/SeriesController.cs
@@ -7,8 +7,8 @@
     using AutoMapper;
 
     using Services;
-    using Services.Models;
-    using Web.Models;
+    using Services.Models.Series;
+    using Web.Models.Series;
     using Data;
 
     public class SeriesController : Controller
@@ -16,17 +16,83 @@
         private readonly IMapper mapper;
         private readonly EverythingNBADbContext db;
         private readonly ISeriesService seriesService;
+        private readonly ITeamService teamService;
+        private readonly IPlayoffService playoffService;
 
-        public SeriesController(IMapper mapper, EverythingNBADbContext db, ISeriesService seriesService)
+        public SeriesController(IMapper mapper, EverythingNBADbContext db, ISeriesService seriesService, ITeamService teamService, 
+            IPlayoffService playoffService)
         {
             this.mapper = mapper;
             this.db = db;
             this.seriesService = seriesService;
+            this.teamService = teamService;
+            this.playoffService = playoffService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> SeriesOverview(int seriesId, string conference, string stage)
         {
-            return View();
+            var seriesModel = await this.seriesService.GetSeriesAsync(seriesId);
+            seriesModel.Conference = conference;
+            seriesModel.Stage = stage;
+
+            ViewBag.WinnerName = await this.seriesService.GetWinnerAsync(seriesId);
+
+            return View(seriesModel);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SeriesInputModel inputModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View(inputModel);
+            }
+
+            var team1 = await this.teamService.GetTeamDetailsAsync(inputModel.Team1Name);
+            var team2 = await this.teamService.GetTeamDetailsAsync(inputModel.Team2Name);
+
+            if (team1 == null || team2 == null)
+            {
+                return this.View(inputModel);
+            }
+
+
+            var seriesId = await this.seriesService.AddSeriesAsync(inputModel.PlayoffId ,team1.Id, team2.Id, inputModel.Team1GamesWon, inputModel.Team2GamesWon,
+                            inputModel.Game1Id, inputModel.Game2Id, inputModel.Game3Id, inputModel.Game4Id, inputModel.Game5Id,
+                            inputModel.Game6Id, inputModel.Game7Id, inputModel.Conference, inputModel.Stage, inputModel.StageNumber);
+
+            await this.playoffService.AddSeriesAsync(inputModel.PlayoffId, seriesId);
+
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int seriesId)
+        {
+            var series = await this.seriesService.GetSeriesAsync(seriesId);
+
+            if (series == null)
+            {
+                return this.View();
+            }
+
+            return this.View(series);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int seriesId, SeriesInputModel model)
+        {
+            var series = await this.seriesService.GetSeriesAsync(seriesId);
+
+            await this.seriesService.DeleteSeriesAsync(seriesId);
+
+            return RedirectToAction("PlayoffBracket", "Playoffs");
         }
     }
 }
---
+++ EverythingNBA.Web/Models/Series/SeriesInputModel.cs
@@ -0,0 +1,55 @@
+﻿namespace EverythingNBA.Web.Models.Series
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static EverythingNBA.Models.Utilities.DataConstants;
+    using EverythingNBA.Models.Enums;
+    using Web.Utilities.Enums;
+
+    public class SeriesInputModel
+    {
+        public int Id { get; set; }
+
+        public int PlayoffId { get; set; }
+
+        [MaxLength(MaxTeamName)]
+        [MinLength(MinTeamName)]
+        public string Team1Name { get; set; }
+
+        [MaxLength(MaxTeamName)]
+        [MinLength(MinTeamName)]
+        public string Team2Name { get; set; }
+
+        [Range(0, 4)]
+        public int Team1GamesWon { get; set; }
+
+        [Range(0, 4)]
+        public int Team2GamesWon { get; set; }
+
+        public int? Game1Id { get; set; }
+
+        public int? Game2Id { get; set; }
+
+        public int? Game3Id { get; set; }
+
+        public int? Game4Id { get; set; }
+
+        public int? Game5Id { get; set; }
+
+        public int? Game6Id { get; set; }
+
+        public int? Game7Id { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(ConferenceType))]
+        public string Conference { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(PlayoffStageType))]
+        public string Stage { get; set; }
+
+        [Required]
+        [Range(1, 4)]
+        public int StageNumber { get; set; }
+    }
+}
