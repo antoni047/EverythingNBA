a95135f00bc73b89c5bb03848beccdd6af77544a
Antonio <antniopatsov477@gmail.com>
2020-03-15 19:48:43 +0200

    Added 3 bool properties to playoff to check the stage of playoff

---
+++ EverythingNBA.Data/Migrations/20200315174609_AddedCheckPlayoff.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EverythingNBA.Data.Migrations
+{
+    public partial class AddedCheckPlayoff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AreConferenceFinalsFinished",
+                table: "Playoffs",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AreQuarterFinalsFinished",
+                table: "Playoffs",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AreSemiFinalsFinished",
+                table: "Playoffs",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreConferenceFinalsFinished",
+                table: "Playoffs");
+
+            migrationBuilder.DropColumn(
+                name: "AreQuarterFinalsFinished",
+                table: "Playoffs");
+
+            migrationBuilder.DropColumn(
+                name: "AreSemiFinalsFinished",
+                table: "Playoffs");
+        }
+    }
+}
--- EverythingNBA.Models/Playoff.cs
+++ EverythingNBA.Models/Playoff.cs
@@ -6,49 +6,15 @@
     {
         public int Id { get; set; }
 
-
         public int? SeasonId { get; set; }
         public virtual Season Season { get; set; }
 
+        public bool AreQuarterFinalsFinished { get; set; }
+
+        public bool AreSemiFinalsFinished { get; set; }
+
+        public bool AreConferenceFinalsFinished { get; set; }
+
         public ICollection<Series> Series { get; set; }
-
-        //public int? WesternQuarterFinalFirstId { get; set; }
-        //public virtual Series WesternQuarterFinalFirst { get; set; }
-        //public int? WesternQuarterFinalSecondId { get; set; }
-        //public virtual Series WesternQuarterFinalSecond { get; set; }
-        //public int? WesternQuarterFinalThirdId { get; set; }
-        //public virtual Series WesternQuarterFinalThird { get; set; }
-        //public int? WesternQuarterFinalFourthId { get; set; }
-        //public virtual Series WesternQuarterFinalFourth { get; set; }
-
-
-        //public int? EasternQuarterFinalFirstId { get; set; }
-        //public virtual Series EasternQuarterFinalFirst { get; set; }
-        //public int? EasternQuarterFinalSecondId { get; set; }
-        //public virtual Series EasternQuarterFinalSecond { get; set; }
-        //public int? EasternQuarterFinalThirdId { get; set; }
-        //public virtual Series EasternQuarterFinalThird { get; set; }
-        //public int? EasternQuarterFinalFourthId { get; set; }
-        //public virtual Series EasternQuarterFinalFourth { get; set; }
-
-        //public int? WesternSemiFinalFirstId { get; set; }
-        //public virtual Series WesternSemiFinalFirst { get; set; }
-        //public int? WesternSemiFinalSecondId { get; set; }
-        //public virtual Series WesternSemiFinalSecond { get; set; }
-
-        //public int? EasternSemiFinalFirstId { get; set; }
-        //public virtual Series EasternSemiFinalFirst { get; set; }
-        //public int? EasternSemiFinalSecondId { get; set; }
-        //public virtual Series EasternSemiFinalSecond { get; set; }
-
-        //public int? WesternFinalId { get; set; }
-        //public virtual Series WesternFinal { get; set; }
-
-        //public int? EasternFinalId { get; set; }
-        //public virtual Series EasternFinal { get; set; }
-
-        //public int? FinalId { get; set; }
-        //public virtual Series Final { get; set; }
-
     }
 }
