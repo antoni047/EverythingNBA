33084d1d4eb9ad5737c2756b9d51303c3d3e1431
Antonio <antniopatsov477@gmail.com>
2020-03-15 17:05:12 +0200

    AddedS teams standings positions to series

---
+++ EverythingNBA.Data/Migrations/20200315150130_AddedTeamStandingsPositionToSeriesTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EverythingNBA.Data.Migrations
+{
+    public partial class AddedTeamStandingsPositionToSeriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Team1StandingsPosition",
+                table: "Series",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Team2StandingsPosition",
+                table: "Series",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Team1StandingsPosition",
+                table: "Series");
+
+            migrationBuilder.DropColumn(
+                name: "Team2StandingsPosition",
+                table: "Series");
+        }
+    }
+}
--- EverythingNBA.Models/Series.cs
+++ EverythingNBA.Models/Series.cs
@@ -22,9 +22,12 @@
         public int? Team1Id { get; set; }
         public virtual Team Team1 { get; set; }
 
-        
+        public int Team1StandingsPosition { get; set; }
+
         public int? Team2Id { get; set; }
         public virtual Team Team2 { get; set; }
+
+        public int Team2StandingsPosition { get; set; }
 
         public int? Team1GamesWon { get; set; }
         public int? Team2GamesWon { get; set; }
