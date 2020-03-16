13ef0d9368757324c06ec03fe6712cb97f34728f
Antonio <antniopatsov477@gmail.com>
2020-03-16 12:19:13 +0200

    Added first names of teams to GetFullTeamName method

--- EverythingNBA.Web/Controllers/TeamsController.cs
+++ EverythingNBA.Web/Controllers/TeamsController.cs
@@ -42,7 +42,7 @@
         public async Task<IActionResult> TeamDetails(string teamName)
         {
             //checking if the name is an abbreviation
-            string fullName = teamName.Length == 3 ? this.GetFullTeamName(teamName) : teamName;
+            string fullName = this.GetFullTeamName(teamName);
 
             var currentYear = this.GetCurrentSeasonYear();
             var teamDetailsModel = await this.teamService.GetTeamDetailsAsync(fullName, currentYear);
@@ -154,71 +154,197 @@
 
         private string GetFullTeamName(string shortName)
         {
-            switch (shortName)
+            if (shortName == "ATL" || shortName == "Atlanta")
             {
-                case "ATL":
-                    return "Atlanta Hawks";
-                case "BKN":
-                    return "Brooklyn Nets";
-                case "BOS":
-                    return "Boston Celtics";
-                case "CHA":
-                    return "Charlotte Hornets";
-                case "CHI":
-                    return "Chicago Bulls";
-                case "CLE":
-                    return "Cleveland Cavaliers";
-                case "DAL":
-                    return "Dallas Mavericks";
-                case "DEN":
-                    return "Denver Nuggets";
-                case "DET":
-                    return "Detroit Pistons";
-                case "GSW":
-                    return "Golden State Warriors";
-                case "HOU":
-                    return "Houston Rockets";
-                case "IND":
-                    return "Indiana Pacers";
-                case "LAC":
-                    return "Los Angeles Clippers";
-                case "LAL":
-                    return "Los Angeles Lakers";
-                case "MEM":
-                    return "Memphis Grizzlies";
-                case "MIA":
-                    return "Miami Heat";
-                case "MIL":
-                    return "Milwaukee Bucks";
-                case "MIN":
-                    return "Minnesota Timberwolves";
-                case "NOP":
-                    return "New Orleans Pelicans";
-                case "NYK":
-                    return "New York Knicks";
-                case "OKC":
-                    return "Oklahoma City Thunder";
-                case "ORL":
-                    return "Orlando Magic";
-                case "PHI":
-                    return "Philadelphia 76ers";
-                case "POR":
-                    return "Portland Trail Blazers";
-                case "SAC":
-                    return "Sacramento Kings";
-                case "TOR":
-                    return "Toronto Raptors";
-                case "UTA":
-                    return "Utah Jazz";
-                case "WAS":
-                    return "Washington Wizards";
-                case "PHX":
-                    return "Phoenix Suns";
-                case "SAS":
-                    return "San Antonio Spurs";
-                default:
-                    return "Error";
+                return "Atlanta Hawks";
             }
+            else if (shortName == "BKN" || shortName == "Brooklyn")
+            {
+                return "Brooklyn Nets";
+            }
+            else if(shortName == "BOS" || shortName == "Boston")
+            {
+                return "Boston Celtics";
+            }
+            else if (shortName == "CHA" || shortName == "Charlotte")
+            {
+                return "Charlotte Hornets";
+            }
+            else if (shortName == "CHI" || shortName == "Chicago")
+            {
+                return "Chicago Bulls";
+            }
+            else if (shortName == "CLE" || shortName == "Cleveland")
+            {
+                return "Cleveland Cavaliers";
+            }
+            else if (shortName == "DAL" || shortName == "Dallas")
+            {
+                return "Dallas Mavericks";
+            }
+            else if (shortName == "DEN" || shortName == "Denver")
+            {
+                return "Denver Nuggets";
+            }
+            else if (shortName == "DET" || shortName == "Detroit")
+            {
+                return "Detroit Pistons";
+            }
+            else if (shortName == "GSW" || shortName == "Golden State")
+            {
+                return "Golden State Warriors";
+            }
+            else if (shortName == "HOU" || shortName == "Houston")
+            {
+                return "Houston Rockets";
+            }
+            else if (shortName == "IND" || shortName == "Indiana")
+            {
+                return "Indiana Pacers";
+            }
+            else if (shortName == "LAC" || shortName == "Los Angeles")
+            {
+                return "Los Angeles Clippers";
+            }
+            else if (shortName == "LAL" || shortName == "Los Angeles")
+            {
+                return "Los Angeles Lakers";
+            }
+            else if (shortName == "MEM" || shortName == "Memphis")
+            {
+                return "Memphis Grizzlies";
+            }
+            else if (shortName == "MIA" || shortName == "Miami")
+            {
+                return "Miami Heat";
+            }
+            else if (shortName == "MIL" || shortName == "Milwaukee")
+            {
+                return "Milwaukee Bucks";
+            }
+            else if (shortName == "MIN" || shortName == "Minnesota")
+            {
+                return "Minnesota Timberwolves";
+            }
+            else if (shortName == "NOP" || shortName == "New Orleans")
+            {
+                return "New Orleans Pelicans";
+
+            }
+            else if (shortName == "NYK" || shortName == "New York")
+            {
+                return "New York Knicks";
+            }
+            else if (shortName == "OKC" || shortName == "Oklahoma City")
+            {
+                return "Oklahoma City Thunder";
+            }
+            else if (shortName == "ORL" || shortName == "Orlando")
+            {
+                return "Orlando Magic";
+            }
+            else if (shortName == "PHI" || shortName == "Philadelphia")
+            {
+                return "Philadelphia 76ers";
+            }
+            else if (shortName == "POR" || shortName == "Portland Trail")
+            {
+                return "Portland Trail Blazers";
+            }
+            else if (shortName == "SAC" || shortName == "Sacramento ")
+            {
+                return "Sacramento Kings";
+            }
+            else if (shortName == "TOR" || shortName == "Toronto")
+            {
+                return "Toronto Raptors";
+            }
+            else if (shortName == "UTA" || shortName == "Utah")
+            {
+                return "Utah Jazz";
+            }
+            else if (shortName == "WAS" || shortName == "Washington")
+            {
+                return "Washington Wizards";
+            }
+            else if (shortName == "PHO" || shortName == "Phoenix")
+            {
+                return "Phoenix Suns";
+            }
+            else if (shortName == "SAS" || shortName == "San Antonio")
+            {
+                return "San Antonio Spurs";
+            }
+            else
+            {
+                return "Error";
+            }
+
+            //switch (shortName)
+            //{
+            //    case "ATL":
+            //        return "Atlanta Hawks";
+            //    case "BKN":
+            //        return "Brooklyn Nets";
+            //    case "BOS":
+            //        return "Boston Celtics";
+            //    case "CHA":
+            //        return "Charlotte Hornets";
+            //    case "CHI":
+            //        return "Chicago Bulls";
+            //    case "CLE":
+            //        return "Cleveland Cavaliers";
+            //    case "DAL":
+            //        return "Dallas Mavericks";
+            //    case "DEN":
+            //        return "Denver Nuggets";
+            //    case "DET":
+            //        return "Detroit Pistons";
+            //    case "GSW":
+            //        return "Golden State Warriors";
+            //    case "HOU":
+            //        return "Houston Rockets";
+            //    case "IND":
+            //        return "Indiana Pacers";
+            //    case "LAC":
+            //        return "Los Angeles Clippers";
+            //    case "LAL":
+            //        return "Los Angeles Lakers";
+            //    case "MEM":
+            //        return "Memphis Grizzlies";
+            //    case "MIA":
+            //        return "Miami Heat";
+            //    case "MIL":
+            //        return "Milwaukee Bucks";
+            //    case "MIN":
+            //        return "Minnesota Timberwolves";
+            //    case "NOP":
+            //        return "New Orleans Pelicans";
+            //    case "NYK":
+            //        return "New York Knicks";
+            //    case "OKC":
+            //        return "Oklahoma City Thunder";
+            //    case "ORL":
+            //        return "Orlando Magic";
+            //    case "PHI":
+            //        return "Philadelphia 76ers";
+            //    case "POR":
+            //        return "Portland Trail Blazers";
+            //    case "SAC":
+            //        return "Sacramento Kings";
+            //    case "TOR":
+            //        return "Toronto Raptors";
+            //    case "UTA":
+            //        return "Utah Jazz";
+            //    case "WAS":
+            //        return "Washington Wizards";
+            //    case "PHX":
+            //        return "Phoenix Suns";
+            //    case "SAS":
+            //        return "San Antonio Spurs";
+            //    default:
+            //        return "Error";
+            //}
         }
     }
 }
