5b5efdc69be14f6910c1d336e39cd18f386b6cfe
Antonio <antniopatsov477@gmail.com>
2020-03-18 15:08:21 +0200

    Added a team listing service model

---
+++ EverythingNBA.Services/Models/Team/TeamListingSerivceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EverythingNBA.Services.Models.Team
+{
+    public class TeamListingSerivceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
