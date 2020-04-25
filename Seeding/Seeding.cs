846cac00787a1a6ffe3ef267a5fec4b28e44fa1b
Antonio <antoniopatsov477@gmail.com>
2020-04-25 11:34:19 +0300

    Added missing seeding project

---
+++ Seeding/Seeding.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seeding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
